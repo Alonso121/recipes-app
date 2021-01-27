c122f99e092f4f8856079e582f7afb53b263f827
Alison <alison.tahiri@gmail.com>
2021-01-27 11:11:51 +0100

    improved env configs

--- src/Form.js
+++ src/Form.js
@@ -3,7 +3,6 @@ import CaloriesInput from "./FormComp/CaloriesInput";
 import DietInput from "./FormComp/DietInput";
 import MainInput from "./FormComp/MainInput";
 import Recipe from "./Recipe";
-require("dotenv").config();
 
 function Recipes() {
   const [foodInfo, setFoodInfo] = useState({
@@ -18,7 +17,7 @@ function Recipes() {
 
   useEffect(() => {
     fetch(
-      `https://api.edamam.com/search?q=${foodInfo.foodType}&app_id=${process.env.APP_ID}&app_key=${process.env.APP_KEY}&calories=${foodInfo.inquireCal}&diet=${foodInfo.diet}&to=30`
+      `${process.env.REACT_APP_EDAMAM_URL}?q=${foodInfo.foodType}&app_id=${process.env.REACT_APP_EDAMAM_ID}&app_key=${process.env.REACT_APP_EDAMAM_KEY}&calories=${foodInfo.inquireCal}&diet=${foodInfo.diet}&to=30`
     )
       .then((res) => res.json())
       .then((result) => {
